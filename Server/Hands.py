546cf3ef77d9ff467cef9728f228a8717b72a847
Ritwik Dutta <ritzymail@gmail.com>
2020-04-05 23:42:34 -0700

    Updating code and working on site.

--- Server/Hands.py
+++ Server/Hands.py
@@ -1,24 +1,23 @@
-import json
-
 from flask import *
-from Objects.Cards import *
+from flask_cors import CORS, cross_origin
+from Simulator import *
 
 app = Flask(__name__)
-
-hand_chances_db = 'card-probabilities-old.json'
-hand_chances = {}
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-def load_hand_probabilities(probability_db):
-    with open(probability_db, 'r') as file_handler:
-        temp_chances = json.loads(file_handler.read())
-    return temp_chances
-
-@app.route('/api/hand-chances/', methods=['GET'])
+@cross_origin()
+@app.route('/api/hand-chances/', methods=['POST'])
 def get_hand_chance():
-    raw_hand = request.data
-    hand = raw_hand.decode('utf-8')
-    return hand_chances[hand]
+    rq = request.json
+    print(rq)
+    hand = [tuple(card) for card in rq['hand']]
+    board = [tuple(card) for card in rq['board']]
+    runs = rq['runs']
+    print(runs)
+    output = simulate_hand_probability(hand, runs, board)
+    print(output)
+    return jsonify(output)
 
 
 @app.route('/')
@@ -27,6 +26,4 @@ def hands():
 
 
 if __name__ == '__main__':
-    hand_chances = load_hand_probabilities(hand_chances_db)
-    print(hand_chances)
     app.run()
